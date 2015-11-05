06ab0a06f0b9feba5f37510254ed168257ca3ebc
fel486 <feleast.49@gmail.com>
2015-11-05 17:18:01 -0300

    att

--- Php/bin.php
+++ Php/bin.php
@@ -11,7 +11,7 @@ require_once('functions.php');
 function bin($vector)
 {
     $bucket = array();
-    for($a = 0; $a < count($vector)+1; $a++) array_push($bucket, 0);
+    for($a = 0; $a < max($vector)+1; $a++) array_push($bucket, 0);
     for($a = 0; $a < count($vector); $a++)  $bucket[$vector[$a]]++;
     for($a = 0; $a < count($vector); $a++)    
        for($b = 0; $b < $bucket[$a]; $b++)
