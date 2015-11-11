1730b1388e023bf8ab334ae3a2f5a6b843a68a04
fel486 <feleast.49@gmail.com>
2015-11-10 22:02:14 -0300

    att

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
 <script src="scripts/js/jquery-1.11.3.min.js"></script>
 <script src="scripts/js/bootstrap.min.js"></script>
 <script src="scripts/js/Chart.min.js"></script>
+<script src="scripts/js/angular-chart.min.js"></script>
+
 <link rel="stylesheet" href="scripts/css/bootstrap.min.css">
+<link rel="stylesheet" href="scripts/css/angular-chart.min.css">
 
 <?php 
 
@@ -22,11 +25,60 @@
 ?>
 
 <script>
-	var app = angular.module('MyApp', []);
+	var app = angular.module('MyApp', ['chart.js']);
 	var current_actived = null;
 
 	app.controller('OptionsController', function($scope) 
 	{
+		$scope.charts = [
+			{
+				ID: "Modal0",
+				Name: "Shell Sort",
+				Label: [1, 2, 3, 4, 5], // Label
+				Serie: ["Serie A"],	// Serie
+				Data: [[20,40,10,25,49],], // Data
+				Previous: "#Modal4",
+				Next: "#Modal1"
+			},
+			{
+				ID: "Modal1",
+				Name: "Quick Sort",
+				Label: [1, 2, 3, 4, 5, 6], // Label
+				Serie: ["Serie A"],	// Serie
+				Data: [[1, 3, 5, 7, 9],], // Data
+				Previous: "#Modal0",
+				Next: "#Modal2"
+			},
+			{
+				ID: "Modal2",
+				Name: "Merge Sort",
+				Label: [1, 2, 3, 4, 5], // Label
+				Serie: ["Serie A"],	// Serie
+				Data: [[20,40,10,25,49],], // Data
+				Previous: "#Modal1",
+				Next: "#Modal3"
+			},
+			{
+				ID: "Modal3",
+				Name: "Bin Sort",
+				Label: [1, 2, 3, 4, 5, 6], // Label
+				Serie: ["Serie A"],	// Serie
+				Data: [[1, 3, 5, 7, 9],], // Data
+				Previous: "#Modal2",
+				Next: "#Modal4"
+			},
+			{
+				ID: "Modal4",
+				Name: "Radix Sort",
+				Label: [1, 2, 3, 4, 5, 6], // Label
+				Serie: ["Serie A"],	// Serie
+				Data: [[1, 3, 5, 7, 9],], // Data
+				Previous: "#Modal3",
+				Next: "#Modal0"
+			},
+
+		];		
+
 		$scope.buttons = [
 			{Class: "btn btn-lg btn-info", Model: "SS", Value: "Shell Sort", Id: "Shell.php", Actived: false},
 			{Class: "btn btn-lg btn-info", Model: "QS", Value: "Quick Sort", Id: "Quick.php", Actived: false},
@@ -40,15 +92,7 @@
 			{ID: "repeat", Model: "repeat", Name: "repeat", Type: 'checkbox', Class: '', Value: "", Label: "Repetir números?", Click: ""},
 			{ID: "decreased", Model: "decreased", Name: "decreased", Type: 'checkbox', Class: '', Value: "", Label: "Ordenar Decrementalmente?", Click: ""},
 			{ID: "submit", Model: "submit", Name: "post", Type: 'button', Class: 'btn btn-lg btn-success', Value: "-", Label: "", Click: ""}
-		];
-
-		$scope.modals = [
-			{ID: "graphicsModal0", Previous: "#graphicsModal4", Next: "#graphicsModal1", Content: "1", Div: "chart0"},
-			{ID: "graphicsModal1", Previous: "#graphicsModal0", Next: "#graphicsModal2", Content: "2", Div: "chart1"},
-			{ID: "graphicsModal2", Previous: "#graphicsModal1", Next: "#graphicsModal3", Content: "3", Div: "chart2"},
-			{ID: "graphicsModal3", Previous: "#graphicsModal2", Next: "#graphicsModal4", Content: "4", Div: "chart3"},
-			{ID: "graphicsModal4", Previous: "#graphicsModal3", Next: "#graphicsModal0", Content: "5", Div: "chart4"},
-		];
+		];		
 
 		$scope.menu = {title: "", text: ""};
 		$scope.resposta = {title: "-", text: "-"};
@@ -59,6 +103,12 @@
 		$scope.getActiveButton = function() {
 			for(var a = 0; a < $scope.buttons.length; a++) {
 				if($scope.buttons[a].Actived) return $scope.buttons[a].Id;
+			}				
+		}
+
+		$scope.getButtonValue = function() {
+			for(var a = 0; a < $scope.buttons.length; a++) {
+				if($scope.buttons[a].Actived) return $scope.buttons[a].Value;
 			}				
 		}
 
@@ -104,6 +154,9 @@
 						$(this).fadeIn(1000);
 					});
 
+					$scope.charts($scope.getButtonValue();
+
+
 					$scope.resposta.title = "Resultados do procedimento";
 					$scope.resposta.text = "Tempo de processamento: "+data+" ms.";
 				}
@@ -136,17 +189,15 @@
 				};
 
 				dataList[a] = data;
-				var div = "chart"+a;
-				console.log(div);
-				var element = document.getElementById(div).getContext("2d");
+				var div = "chart"+a;				
+				var element = document.getElementById("chart0").getContext("2d");
 				chartList[a] = new Chart(element).Line(dataList[a], {animationSteps: 15});							
 			}		
-		}		
+		}				
 	});
 	
 	//var quant = 5;
 	
-
 	function test() {
 		var data = 
 			{
@@ -169,6 +220,10 @@
 		var gc = new Chart(element).Line(data, {responsive: false});
 	}
 
+	$("#graphicsModal0").on('shown.bs.modal', function(event) {
+
+	});
+
 	function add(time) 
 	{		
 		gc.addData([document.getElementById('digitador').value], data.labels.length+1);  		  		
@@ -188,13 +243,13 @@
 <body ng-app="MyApp" onLoad="">
 
 <div class="navbar navbar-default" style="background-color: orange; box-shadow: 1px 1px 1px #000">
-	<div style='font-size: 36px; text-align: center; font-family: "Courier new"'>Trabalho de Construção de Algoritmos</div>
+	<div style='font-size: 36px; text-align: center; font-family: "trebuchet"'>Trabalho de Construção de Algoritmos</div>
 </div>
 
 <div ng-controller="OptionsController">
 	<center><span ng-repeat="button in buttons" ng-init="select(buttons[0]); loadGraphics()">
 		<input type='button' ng-click="select(button); " ng-model="button.Model" class={{button.Class}} value={{button.Value}}>		
-	</span><br><br><input type='button' id='btn-graphics' data-toggle='modal' data-target='#graphicsModal0' class='btn btn-lg btn-warning' value='Graphics!'>
+	</span><br><br><input type='button' id='btn-graphics' data-toggle="modal" data-target="#Modal0" class='btn btn-lg btn-warning' value='Graphics!'>
 	</center><hr>
 	<div ng-model='menu'>
 		<center><h2>{{menu.title}}</h2></center>
@@ -207,22 +262,25 @@
 		<div>{{resposta.text}}</div>
 	</div></center><hr>
 
-	<span ng-repeat="modal in modals"> 
-		<div class='modal fade' id='{{modal.ID}}' role="dialog">
+	<div ng-repeat="chart in charts">
+		<div class='modal fade' id="{{chart.ID}}">
 			<div class='modal-dialog'>
 				<div class='modal-content'>
+					<div class='modal-header' style='font-size: 20px'>Algoritmo de {{chart.Name}}</div>
 					<div class='modal-body'>
-						<canvas id='{{modal.Div}}' width="200" height="200"></canvas>
+						<canvas id="chart.ID" width="500%" class="chart chart-line" chart-data="chart.Data" chart-labels="chart.Label"></canvas>
 					</div>
 					<div class='modal-footer'>
-						<input type="button" class='btn btn-lg btn-success' value="Previous Chart" data-toggle='modal' data-target='{{modal.Previous}}' data-dismiss='modal'>
-						<input type="button" class='btn btn-lg btn-info' value="Previous Next" data-toggle='modal' data-target='{{modal.Next}}' data-dismiss='modal'>
+						<button class='btn-lg btn-info' data-dismiss='modal' data-toggle='modal' data-target="{{chart.Previous}}">Previous Chart</button>
+						<button class='btn-lg btn-success' data-dismiss='modal' data-toggle='modal' data-target="{{chart.Next}}">Next Chart</button>
 					</div>
-				</div>
-			</div>
-		</div>
-	</span>
+				</div>				
+			</div>		
+		</div>		
+	</div>	
 
+	<!-- <input type="number" ng-model="inputer">
+	<button ng-click="d[0].push(inputer); l.push(l[l.length-1]+1)">Added</button> -->
 </div>
 
 </body>
